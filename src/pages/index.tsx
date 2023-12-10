d1b53831d091b01c17e04cf4b58a3bdc9a2d3576
teradonburi <daikiterai@gmail.com>
2023-12-10 15:48:48 +0900

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 
 import { Partytown } from '@builder.io/partytown/react'
+import Head from 'next/head';
 const gtmScript = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
@@ -13,27 +14,30 @@ export const config = {
 export default function Home() {
   return (
     <>
-      <Partytown
-        debug={true}
-        forward={['dataLayer.push']}
-        resolveUrl={(url, location) => {
+      <Head>
+        <Partytown
+          debug={true}
+          forward={['dataLayer.push']}
+          resolveUrl={(url, location) => {
 
-          // CORS 対策でrequestをreverse proxyする
-          if (url.href.startsWith('https://')) {
-            const host = url.host
-            const path = url.pathname === '/' ? '' : url.pathname
-            const search = url.search === '?' ? '' : url.search
-            const proxyUrl = new URL(location.origin + '/proxy' + path + search)
-            proxyUrl.searchParams.append(
-              'target_party_host',
-              host
-            )
-            return proxyUrl
-          }
-          return url
-        }}
-      />      {/** Google Tag Manager */}
-      <script type="text/partytown" defer dangerouslySetInnerHTML={{__html: gtmScript}}></script>
+            // CORS 対策でrequestをreverse proxyする
+            if (url.href.startsWith('https://')) {
+              const host = url.host
+              const path = url.pathname === '/' ? '' : url.pathname
+              const search = url.search === '?' ? '' : url.search
+              const proxyUrl = new URL(location.origin + '/proxy' + path + search)
+              proxyUrl.searchParams.append(
+                'target_party_host',
+                host
+              )
+              return proxyUrl
+            }
+            return url
+          }}
+        />     
+        {/** Google Tag Manager */}
+        <script type="text/partytown" defer dangerouslySetInnerHTML={{__html: gtmScript}}></script>
+      </Head>
       <div>test partytown</div>      
     </>
   )
