cb27d2d7064d8542bde678debe831e68b7f54951
teradonburi <daikiterai@gmail.com>
2023-12-10 16:23:56 +0900

    partial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,7 +39,7 @@ export default function Home() {
           }}
         />     
         {/** Google Tag Manager */}
-        <script defer dangerouslySetInnerHTML={{__html: gtmScript}}></script>
+        <script type="text/partytown" defer dangerouslySetInnerHTML={{__html: gtmScript}}></script>
       </Head>
       <div>test partytown</div>      
     </>
